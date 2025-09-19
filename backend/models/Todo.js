8586967fae3f05f812a7cc8b7aa0cc00cdaa7592
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2025-09-19 22:26:13 +0530

    Update Todo.js

--- backend/models/Todo.js
+++ backend/models/Todo.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
 
-const todoSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  completed: { type: Boolean, default: false }
-}, { timestamps: true });
+const todoSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    completed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("Todo", todoSchema);
+export default mongoose.models.Todo || mongoose.model("Todo", todoSchema);
