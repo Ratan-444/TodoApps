a1413c8a8bb70939c073146763404cf67c2309dd
Ratan-444 <131332225+Ratan-444@users.noreply.github.com>
2025-09-20 22:39:29 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import todoRoutes from "./routes/todoRoutes.js";
+import todoRoutes from "./routes/todos.js";
 
 dotenv.config();
 const app = express();
